e106728f0287bd32c6e40846c2ecefac4d8c479e
Mohamed Atef <proofessergmail.com>
2022-10-02 20:27:37 +0100

    using ViewModel on the view.

--- app/src/main/java/mo/ed/amit/dayten/network/view/fragment/ProfilesFragment.java
+++ app/src/main/java/mo/ed/amit/dayten/network/view/fragment/ProfilesFragment.java
@@ -1,6 +1,7 @@
 package mo.ed.amit.dayten.network.view.fragment;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,15 +11,21 @@ import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
 import androidx.databinding.ViewDataBinding;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import java.util.List;
 
 import mo.ed.amit.dayten.network.R;
 import mo.ed.amit.dayten.network.databinding.ProfilesFragmentBinding;
 import mo.ed.amit.dayten.network.room.model.profiles.Profile;
+import mo.ed.amit.dayten.network.viewmodel.ProfileViewModel;
 
 public class ProfilesFragment extends Fragment {
 
 
     private ProfilesFragmentBinding binding;
+    private ProfileViewModel profilesViewModel;
 
     public static ProfilesFragment newInstance(){
         return new ProfilesFragment();
@@ -30,5 +37,21 @@ public class ProfilesFragment extends Fragment {
         binding=DataBindingUtil.inflate(inflater, R.layout.profiles_fragment,container,false);
 
         return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        profilesViewModel = new ViewModelProvider(this).get(ProfileViewModel.class);
+        profilesViewModel.getApiProfiles().observe(getViewLifecycleOwner(), new Observer<List<Profile>>() {
+            @Override
+            public void onChanged(List<Profile> profiles) {
+                if (profiles!=null){
+                    for (Profile profile : profiles){
+                        Log.e("Profiles", profile.getEmail()+" "+profile.getName());
+                    }
+                }
+            }
+        });
     }
 }
